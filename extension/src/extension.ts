18028355622a68fef53a6d97da5d2f3551fc46d2
EZ <31062364+EagleZhen@users.noreply.github.com>
2025-10-28 15:24:22 +0800

    feat: add initial command registrations

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -1,5 +1,36 @@
 import * as vscode from "vscode";
 
-export function activate(context: vscode.ExtensionContext) {}
+export function activate(context: vscode.ExtensionContext) {
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("thoughtflow.visualizeCallGraph", async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showInformationMessage("Open a Python file and place cursor on a function.");
+        return;
+      }
+
+      // TODO: Full flow (analyze + transform + visualize)
+    })
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("thoughtflow.debug.testAnalyzer", async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showInformationMessage("Open a Python file and place cursor on a function.");
+        return;
+      }
+
+      // TODO: Test analyzer, log to output channel
+    })
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("thoughtflow.debug.testGraph", async () => {
+      // TODO: Test graph with stub data
+    })
+  );
+}
 
 export function deactivate() {}
