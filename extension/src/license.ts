df9607250e50f6d2f69bc1721ac59816baddb947
EZ <31062364+EagleZhen@users.noreply.github.com>
2025-11-05 01:57:13 +0800

    fix: remove analytics to avoid using browser API
    ReferenceError: window is not defined

--- extension/src/license.ts
+++ extension/src/license.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -31,7 +30,6 @@ function initializeFirebaseApp(): FirebaseApp {
     return app;
   }
   app = initializeApp(firebaseConfig);
-  getAnalytics(app);
   return app;
 }
 
