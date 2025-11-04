7d54ef90181bc0c0d9d8c4bff4eb971dac39212b
EZ <31062364+EagleZhen@users.noreply.github.com>
2025-11-05 00:16:10 +0800

    chore: add Firestore config

--- extension/src/license.ts
+++ extension/src/license.ts
@@ -1,2 +1,21 @@
-// User and license management
-// Will handle GitHub authentication, license validation, and API communication with license-service
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAXQmIxOGQ5Szxnh6v1uxKxt4D52T1rO8I",
+  authDomain: "csci3100-thought-flow.firebaseapp.com",
+  projectId: "csci3100-thought-flow",
+  storageBucket: "csci3100-thought-flow.firebasestorage.app",
+  messagingSenderId: "312181637733",
+  appId: "1:312181637733:web:027af76fec415bdc96bc8c",
+  measurementId: "G-92KX2F49TV",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
