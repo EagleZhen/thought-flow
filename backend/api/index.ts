bed09fc336be4d7d9194a61d22fdfed02bff0752
EZ <31062364+EagleZhen@users.noreply.github.com>
2025-11-11 15:56:42 +0800

    chore: remove the unnecessary user-agent
    i can still get a proper response without it

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -14,7 +14,6 @@ async function verifyGitHubToken(
     const response = await fetch("https://api.github.com/user", {
       headers: {
         Authorization: `Bearer ${githubToken}`,
-        "User-Agent": "ThoughtFlow",
       },
     });
 
